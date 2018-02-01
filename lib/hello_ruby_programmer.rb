77f4cf189170cd9c258931943a59b8d639b53560
emi kojima <emi8emi@gmail.com>
2018-02-01 23:22:49 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,4 +1,6 @@
+def greeting(name)
 puts "Hi! Welcome to the wonderful world of Ruby programming."
 puts "Please enter your name so that we can greet you more personally:"
 name = gets.strip
+puts "Hi:"
 greeting(name)
